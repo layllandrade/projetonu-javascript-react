37f1be287427643446e355696c4d0a631de7d0b7
Layllandrade <layllandradef@live.com>
2023-07-28 18:01:47 -0300

    Atualizando barra de busca

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './components/Header/Header.jsx';
 import Products from './components/Products/Products.jsx';
+import Provider from './context/Provider.jsx';
 
 function App() {
   return (
-    <div>
+    <Provider>
       <Header/>
       <Products/>
-    </div>
+    </Provider>
   );
 }
 
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,14 +1,13 @@
-import React, {useState, useEffect} from 'react';
+import React, { useEffect, useContext} from 'react';
 import fetchProducts from '../../api/fetchProducts.js';
 
 import './Products.css';
 import ProductCard from '../ProductCard/ProductCard.jsx';
 import Loading from '../Loading/Loading.jsx';
+import AppContext from '../../context/AppContext.js';
 
 function Products() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
+  const { products, setProducts, loading, setLoading } = useContext(AppContext);
 
   useEffect(() => {
     fetchProducts('iphone').then((response) => {
